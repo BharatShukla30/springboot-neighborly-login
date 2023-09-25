d66b4ead878e399e8d327a3d86ad227551e07bbc
Bharat Shukla <bharatsagii30@gmail.com>
2023-09-25 14:09:01 +0530

    added spring security passowrd hashing

--- src/main/java/com/neighborly/origin/configuration/SecurityConfiguration.java
+++ src/main/java/com/neighborly/origin/configuration/SecurityConfiguration.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -21,13 +23,22 @@ public class SecurityConfiguration {
                         .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().requestMatchers(String.valueOf(PathRequest.toStaticResources().atCommonLocations()));
     }
-
+    @Bean
+    public InMemoryUserDetailsManager users() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("myname")
+                        .password(passwordEncoder().encode("password"))
+                        .authorities("read")
+                        .build()
+        );
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
